a58b6689425fdacbdf0b6408f96cab5eca90a7f2
Eko Kurniawan <setiawanegho@gmail.com>
2022-02-26 22:50:57 +0700

    Add event name setter

--- src/Event.php
+++ src/Event.php
@@ -4,10 +4,13 @@ namespace Ekok\EventDispatcher;
 
 class Event
 {
+    private $name;
     private $propagationStopped = false;
 
-    public function __construct(private string|null $name = null)
-    {}
+    public function __construct(string $name = null)
+    {
+        $this->setName($name);
+    }
 
     public function getName(): string|null
     {
@@ -22,6 +25,13 @@ class Event
     public function stopPropagation(): static
     {
         $this->propagationStopped = true;
+
+        return $this;
+    }
+
+    protected function setName(string|null $name): static
+    {
+        $this->name = $name;
 
         return $this;
     }
